ae19b7783e843d32ea0d18bd88d8c79f06bad028
ryan m <contact@ryanmeek.com>
2020-02-16 21:55:54 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,5 +2,6 @@ class Actor < ActiveRecord::Base
   has_many :characters
 
   def full_name; "#{first_name} #{last_name}" end
+  def list_roles; characters.collect { |c| "#{c.name} - #{c.show.name}" } end
 
 end
