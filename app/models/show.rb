d7d551b01edc7e9c20b5071e714faae471211913
ryan m <contact@ryanmeek.com>
2020-02-16 22:00:10 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,5 +2,8 @@ class Show < ActiveRecord::Base
   has_many :characters
   belongs_to :network
 
+  def actors_list; characters.collect do |c|
+    "#{c.actor.first_name} #{c.actor.last_name}"
+  end
 
 end
